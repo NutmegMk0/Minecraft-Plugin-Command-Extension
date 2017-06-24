2b5b51573185f01671722719bd57f56faaff4554
NutmegMk0 <NutmegMk0@gmail.com>
2017-06-24 14:41:24 +0900

    plugin.ymlを忘れていた件について

--- CommandExtension/src/com/github/nutmegmk0/commandextension/command/CommandBullet.java
+++ CommandExtension/src/com/github/nutmegmk0/commandextension/command/CommandBullet.java
@@ -8,7 +8,9 @@ public class CommandBullet implements CommandExecutor{
 
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO 自動生成されたメソッド・スタブ
+		//コマンドの引数メモ：/Bullet 射出元プレイヤー 設定する名前 飛行速度 消滅までのタイマー 透明かどうか
+		//          それの例：/Bullet NutmegMk0 エクスプロージョン 0.5 100(Tick単位) false(true以外なら何でも透明にはならない)
+
 		return false;
 	}
 
