825e0ff6bed6a7065a30634d8cee2d50f97d66ee
NutmegMk0 <NutmegMk0@gmail.com>
2017-06-24 13:10:24 +0900

    とりあえず空のプラグイン

---
+++ CommandExtension/src/com/github/nutmegmk0/commandextension/main/CommandExtension.java
@@ -0,0 +1,19 @@
+package com.github.nutmegmk0.commandextension.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CommandExtension extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+
+	}
+
+	@Override
+	public void onDisable () {
+
+
+	}
+
+}
