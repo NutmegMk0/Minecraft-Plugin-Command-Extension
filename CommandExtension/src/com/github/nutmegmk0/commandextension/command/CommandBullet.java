251bbdfc1ec2df75ccc833b9fa4f6ec8537c52de
NutmegMk0 <NutmegMk0@gmail.com>
2017-06-24 14:31:08 +0900

    取り合えずクラス追加

---
+++ CommandExtension/src/com/github/nutmegmk0/commandextension/command/CommandBullet.java
@@ -0,0 +1,15 @@
+package com.github.nutmegmk0.commandextension.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandBullet implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		// TODO 自動生成されたメソッド・スタブ
+		return false;
+	}
+
+}
--- CommandExtension/src/com/github/nutmegmk0/commandextension/main/CommandExtension.java
+++ CommandExtension/src/com/github/nutmegmk0/commandextension/main/CommandExtension.java
@@ -2,11 +2,17 @@ package com.github.nutmegmk0.commandextension.main;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.nutmegmk0.commandextension.command.CommandBullet;
+
 public class CommandExtension extends JavaPlugin{
 
 	@Override
 	public void onEnable () {
 
+
+		//コマンド登録
+		CommandBullet comBullet = new CommandBullet();
+		getCommand("bullet").setExecutor(comBullet);
 
 	}
 
