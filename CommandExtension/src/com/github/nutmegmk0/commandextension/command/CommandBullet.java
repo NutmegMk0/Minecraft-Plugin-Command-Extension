f053a74eb19a3e26ba61a92e5a6b320a180a1ffd
NutmegMk0 <NutmegMk0@gmail.com>
2017-06-25 12:41:16 +0900

    とりあえず処理は完成かな...

--- CommandExtension/src/com/github/nutmegmk0/commandextension/command/CommandBullet.java
+++ CommandExtension/src/com/github/nutmegmk0/commandextension/command/CommandBullet.java
@@ -1,11 +1,18 @@
 package com.github.nutmegmk0.commandextension.command;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Silverfish;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import com.github.nutmegmk0.commandextension.main.CommandExtension;
 
 public class CommandBullet implements CommandExecutor{
 
@@ -26,6 +33,40 @@ public class CommandBullet implements CommandExecutor{
 		bullet.setCustomName(arg3[1]);
 
 		bullet.setGravity(false);
+
+		Vector vel = player.getLocation().getDirection();
+
+		//bullet.setVelocity(vel.multiply(Float.parseFloat(arg3[2])));
+
+		bullet.setInvulnerable(true);
+
+		if (arg3[4].equals("true")) {
+
+			PotionEffect effect = new PotionEffect(PotionEffectType.INVISIBILITY, Integer.parseInt(arg3[3]), 1, false, false);
+
+			bullet.addPotionEffect(effect, true);
+		}
+
+		int count = 0;
+
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+
+            	if (count > Integer.parseInt(arg3[3])) {
+            		bullet.damage(10000);
+            		cancel();
+            	}
+
+            	Location loc = bullet.getLocation();
+
+            	loc.add(vel.multiply(Float.parseFloat(arg3[2])));
+
+            	bullet.teleport(loc);
+
+            }
+        }.runTaskTimer(new CommandExtension(), 0, 0);
 
 		return false;
 	}
